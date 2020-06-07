4118c7020e6fb820325306e38328b44f89782bac
Felipe A. J. Zanetti <felipeajz@hotmail.com>
2020-06-07 15:39:54 -0300

    Diretório uploads/points/imagem

--- Server/src/controllers/PointsController.ts
+++ Server/src/controllers/PointsController.ts
@@ -20,7 +20,7 @@ class PointsController {
         const serializedPoints = points.map(point => {
             return {
                 ...point,
-                image_url: `http://192.168.0.108:3333/uploads/${point.image}`
+                image_url: `http://192.168.0.108:3333/uploads/points/${point.image}`
             }
         })
 
@@ -38,7 +38,7 @@ class PointsController {
 
         const serializedPoints = {
             ...point,
-            image_url: `http://192.168.0.108:3333/uploads/${point.image}`
+            image_url: `http://192.168.0.108:3333/uploads/points/${point.image}`
         }
 
         const items = await knex('items')
